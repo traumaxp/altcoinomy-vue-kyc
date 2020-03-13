2b9d2223335deee39267f2588d07f775ed278847
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2020-03-13 20:11:45 +0100

    Clean setup 2

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -1,7 +1,5 @@
 const users = require('./users/users.service.js');
-const transactions = require('./transactions/transactions.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
-  app.configure(transactions);
 };
