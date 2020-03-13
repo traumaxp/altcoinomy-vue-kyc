b679b83643bcf2e054197284e9a9d48f09c0906d
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2020-03-13 21:06:10 +0100

    Update code

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    Home page
+    <h5>Home page</h5>
+    <p>
+      We implement a simple and user-friendly procedure in line with the Swiss regulatory framework,
+      that’s why you are asked to complete a brief « Know Your Customer » process.
+    </p>
+    <p>
+      <strong>
+        Altcoinomy is a Swiss-based supervised KYC operator who will be conducting anti money laundering analysis.
+        Altcoinomy SA (CHE-209.239.695) is bound by Swiss laws on banking secrecy,
+        and will never distribute confidential information about you.
+      </strong>
+    </p>
+    <p>All data is encrypted and stored offline.</p>
+    <p>Let’s get started!</p>
   </div>
 </template>
 
